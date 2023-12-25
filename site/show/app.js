f8a4737c71f9ed6ed79a9a61d3cdd276b49e8ae3
GSemix <bezgin.semen555901@gmail.com>
2023-12-25 04:42:32 +0300

    update page show

--- site/show/app.js
+++ site/show/app.js
@@ -4,29 +4,4 @@ url = new URL(window.location.href);
 
 type_page = url.searchParams.get('num');
 
-document.addEventListener('DOMContentLoaded', () => {
-    const magicTextElements = document.querySelectorAll('.magic-text');
 
-    const elementInView = (el, offset = 0) => {
-        const elementTop = el.getBoundingClientRect().top;
-        return elementTop <= ((window.innerHeight || document.documentElement.clientHeight) - offset);
-    };
-
-    const displayScrollElement = (element) => {
-        element.classList.add('visible');
-    };
-
-    const handleScrollAnimation = () => {
-        magicTextElements.forEach((el) => {
-            if (elementInView(el, 150)) {
-                displayScrollElement(el);
-            }
-        });
-    };
-
-    // Вызываем анимации появления текста при первоначальной загрузке
-    handleScrollAnimation();
-
-    // Отслеживаем событие прокрутки для анимации текста
-    window.addEventListener('scroll', handleScrollAnimation);
-});
